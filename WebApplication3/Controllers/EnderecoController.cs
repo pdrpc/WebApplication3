11f1b86ba6117457b0673a5212e48a34acccf548
pdrpc <pdr.pradocamargo@gmail.com>
2021-04-05 10:27:38 -0300

    Final Commit

--- WebApplication3/Controllers/EnderecoController.cs
+++ WebApplication3/Controllers/EnderecoController.cs
@@ -44,12 +44,31 @@ namespace WebApplication3.Controllers
         }
 
         // GET: Endereco/Create
-        public async Task<ActionResult> Create()
+        public ActionResult Create()
         {
-            Endereco endereco = await ApiRequest.SendAsync("05372120/json");
-            Debug.WriteLine(endereco.Cep + " " +  endereco.Logradouro + " " +  endereco.Complemento + " " +  endereco.Bairro + " " +  endereco.Localidade
-                 + " " +  endereco.Uf + " " +  endereco.Ibge + " " +  endereco.Gia + " " +  endereco.Ddd + " " +  endereco.Siafi);
             return View();
+        }
+
+        public async Task<ActionResult> ApiRequestAsync(string cep)
+        {
+            if (cep != null)
+            {
+                if (cep.Length == 8)
+                {
+                    var endereco = new Endereco();
+                    endereco = await ApiRequest.SendAsync($"{cep}/json");
+                    return View("Create", endereco);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+
         }
 
         // POST: Endereco/Create
@@ -57,17 +76,17 @@ namespace WebApplication3.Controllers
         // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EnderecoId,Cep,Logradouro,Complemento,Bairro,Localidade,Uf,Ibge,Gia,Ddd,Siafi")] Endereco endereco)
+        public ActionResult Create(Endereco model)
         {
+
             if (ModelState.IsValid)
             {
-                endereco.EnderecoId = Guid.NewGuid();
-                db.Enderecoes.Add(endereco);
+                model.EnderecoId = Guid.NewGuid();
+                db.Enderecoes.Add(model);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(endereco);
+            return View();
         }
 
         // GET: Endereco/Edit/5
@@ -92,6 +111,7 @@ namespace WebApplication3.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "EnderecoId,Cep,Logradouro,Complemento,Bairro,Localidade,Uf,Ibge,Gia,Ddd,Siafi")] Endereco endereco)
         {
+            Debug.WriteLine(endereco.Cep);
             if (ModelState.IsValid)
             {
                 db.Entry(endereco).State = EntityState.Modified;
@@ -134,6 +154,21 @@ namespace WebApplication3.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        [HttpGet]
+        public ActionResult CriandoJs() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CriandoJs(Endereco model)
+        {
+                model.EnderecoId = Guid.NewGuid();
+                db.Enderecoes.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            
         }
     }
 }
